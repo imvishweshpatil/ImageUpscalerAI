1b78355da814750b737b7584cefc494d37118455
kidatesting <testing@kidastudios.com>
2023-11-29 18:14:46 +0530

    Initial Commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,238 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useRef } from 'react';
+import Upscaler from 'upscaler';
+import CircularProgress, { CircularProgressProps } from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+const CircularProgressWithLabel = (
+    props: CircularProgressProps & { value: number }
+) => (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+        <CircularProgress variant="determinate" {...props} />
+        <Box
+            sx={{
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                position: 'absolute',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <Typography variant="caption" component="div" color="text.secondary">
+                {`${Math.round(props.value)}%`}
+            </Typography>
+        </Box>
+    </Box>
+);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+const Home: React.FC = () => {
+    const [image, setImage] = useState<string | null>(null);
+    const [upscaledImage, setUpscaledImage] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [patchSize, setPatchSize] = useState(16);
+    const [padding, setPadding] = useState(2);
+    const [spaceBetweenPatches, setSpaceBetweenPatches] = useState(2);
+    const [row, setRow] = useState(0);
+    const [col, setCol] = useState(0);
+    const [progress, setProgress] = useState(0);
+    const [isUpScaling, setIsUpScaling] = useState(false);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    const imgRef = useRef<HTMLImageElement>(null);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImage(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleUpScale = async () => {
+        if (image) {
+            setIsUpScaling(true);
+
+            const upscaler = new Upscaler();
+            const { columns } = getRowsAndColumns(imgRef.current, patchSize);
+
+            const tbody = document.createElement('tbody');
+            let currentRow = document.createElement('tr');
+            tbody.appendChild(currentRow);
+
+            const upscaledImgSrc = await upscaler.upscale(image, {
+                patchSize,
+                padding,
+                /*spaceBetweenPatches,*/
+                progress: (patch, slice) => {
+                    const cell = document.createElement('td');
+                    const sliceImg = new Image();
+                    sliceImg.src = slice;
+                    sliceImg.alt = "Patch ${patch.row}-${patch.col}";
+                    sliceImg.title = "Patch ${patch.row}-${patch.col}";
+                    cell.appendChild(sliceImg);
+
+                    setCol((prevCol) => prevCol + 1);
+
+                    if (col >= columns) {
+                        console.log('new row!');
+                        setRow((prevRow) => prevRow + 1);
+                        setCol(0);
+                        currentRow = document.createElement('tr');
+                        tbody.appendChild(currentRow);
+                    } else {
+                        currentRow.appendChild(cell);
+                    }
+
+                    // Update progress state
+                    setProgress((prevProgress) =>
+                        prevProgress >= 100 ? 0 : prevProgress + 10
+                    );
+                },
+            });
+
+            setIsUpScaling(false);
+            setUpscaledImage(upscaledImgSrc);
+        }
+    };
+
+    const handleDownload = () => {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = upscaledImage || image || '';
+        downloadLink.download = 'upscaled_image.png';
+        downloadLink.click();
+    };
+
+    return (
+        <main>
+            <div className="header">
+                <h1
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        margin: '80px 0px',
+                        color: '#3498DB',
+                        fontSize: '40px',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    Image Upscaler
+                </h1>
+                <div className="image" style={{ textAlign: 'center' }}>
+                    <input type="file" onChange={onChange} accept="image/*" />
+                </div>
+                {image && (
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <img
+                            ref={imgRef}
+                            src={image}
+                            alt="Original Image"
+                            style={{ maxWidth: '100%' }}
+                        />
+                        <div>
+                            <div>
+                                <label>Patch Size</label>
+                                <input
+                                    type="number"
+                                    min="0"
+                                    value={patchSize}
+                                    max="128"
+                                    step="1"
+                                    onChange={(e) => setPatchSize(parseInt(e.target.value))}
+                                />
+                            </div>
+                            <div>
+                                <label>Padding</label>
+                                <input
+                                    type="number"
+                                    min="0"
+                                    value={padding}
+                                    max="20"
+                                    step="1"
+                                    onChange={(e) => setPadding(parseInt(e.target.value))}
+                                />
+                            </div>
+                            <div>
+                                <label>Space Between Patches</label>
+                                <input
+                                    type="number"
+                                    min="0"
+                                    value={spaceBetweenPatches}
+                                    max="40"
+                                    step=".1"
+                                    onChange={(e) =>
+                                        setSpaceBetweenPatches(parseInt(e.target.value))
+                                    }
+                                />
+                            </div>
+                        </div>
+                        {isUpScaling ? (
+                            <CircularProgressWithLabel value={progress} />
+                        ) : (
+                            <button
+                                onClick={handleUpScale}
+                                style={{
+                                    backgroundColor: 'black',
+                                    borderRadius: '10px',
+                                    color: 'white',
+                                    width: '140px',
+                                    margin: '52px 30px',
+                                    height: '25px',
+                                }}
+                            >
+                                Upscale Image
+                            </button>
+                        )}
+                    </div>
+                )}
+
+                {upscaledImage && (
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <img
+                            src={upscaledImage}
+                            alt="Upscaled Image"
+                            style={{ maxWidth: '100%' }}
+                        />
+                        <button
+                            onClick={handleDownload}
+                            style={{
+                                backgroundColor: 'black',
+                                borderRadius: '10px',
+                                color: 'white',
+                                width: '215px',
+                                margin: '52px 30px',
+                                height: '52px',
+                            }}
+                        >
+                            Download Image
+                        </button>
+                    </div>
+                )}
+            </div>
+        </main>
+    );
+};
+
+export default Home;
+
+function getRowsAndColumns(image: HTMLImageElement | null, patchSize: number) {
+    return { rows: 0, columns: 0 };
 }
